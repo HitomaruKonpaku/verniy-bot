e3297db8f79f4a52159988855499a7d81b0cc982
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2017-10-25 19:50:41 +0700

    Add CAL command

---
+++ commands/util/cal.js
@@ -0,0 +1,23 @@
+const { Command } = require('discord.js-commando')
+
+module.exports = class CalculationCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'cal',
+            group: 'util',
+            memberName: 'cal',
+            description: 'Evaluates or executes an argument.',
+            examples: ['cal 1+1', 'cal 2^2'],
+            argsType: 'single',
+        })
+    }
+
+    async run(msg, args) {
+        let pattern = /^[0-9 \+\-\*\/\^()]+$/
+        if (pattern.test(args)) {
+            msg.channel.send(eval(args))
+        } else {
+            msg.channel.send('Invalid!')
+        }
+    }
+}
