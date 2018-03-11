0ca036992a454683bb0eb798cfb33078f65e7e3e
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-03-11 23:33:15 +0700

    Update gitignore

---
+++ _data/settings.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    'Discord': {
+        'ClientID': '422330233035948032',
+        'Token': 'NDIyMzMwMjMzMDM1OTQ4MDMy.DYaWaw.g4RD4pJV2VjNpbVFNaHhRSiDvBE',
+        'Owner': '153363129915539457',
+        'Prefix': '?',
+        'InviteLink': 'https://discordapp.com/oauth2/authorize?client_id=422330233035948032&scope=bot'
+    },
+    'Twitter': {
+        'ConsumerKey': 'C0kUvowIJyonFKicVQf3NeXvB',
+        'ConsumerSecret': 'kfQgoO6sKkgYcWzUGTOhjy3Y9jHKCXwQNqdeRdw3IQ2SzjREq7',
+        'AccessToken': '2591243785-GWwKbj2PhuIKBmm7ruy7Dj34crD9h0ggFEc0weP',
+        'AccessTokenSecret': 'rIDXTtjAYn07yoA4JxxudZgrzuOAdfLiWiaGowHKAyv2n'
+    },
+    'KanColle': {
+        // 'DevTwitterID': '2591243785', // HitomaruKonpaku
+        'DevTwitterID': '294025417', // KanColle_STAFF
+        'BroadcastChannels': [
+            '310270617607864322', // HitoSPAM > botroom
+            '376294828608061440', // Quan nhau cua T > kancolle-only
+            ''
+        ]
+    }
+}
