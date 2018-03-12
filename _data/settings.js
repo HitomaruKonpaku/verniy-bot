9f172f508d3a405b6963680e78fec87f8cef5e8d
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-03-12 23:23:59 +0700

    Refactor settings.js

--- _data/settings.js
+++ _data/settings.js
@@ -1,18 +1,18 @@
 module.exports = {
-    'Discord': {
-        'ClientID': '422330233035948032',
-        'Token': 'NDIyMzMwMjMzMDM1OTQ4MDMy.DYaWaw.g4RD4pJV2VjNpbVFNaHhRSiDvBE',
-        'Owner': '153363129915539457',
-        'Prefix': '.',
-        'InviteLink': 'https://discordapp.com/oauth2/authorize?client_id=422330233035948032&scope=bot'
+    Discord: {
+        ClientID: '422330233035948032',
+        Token: 'NDIyMzMwMjMzMDM1OTQ4MDMy.DYaWaw.g4RD4pJV2VjNpbVFNaHhRSiDvBE',
+        Owner: '153363129915539457',
+        Prefix: '.',
+        InviteLink: 'https://discordapp.com/oauth2/authorize?client_id=422330233035948032&scope=bot'
     },
-    'Twitter': {
-        'ConsumerKey': 'C0kUvowIJyonFKicVQf3NeXvB',
-        'ConsumerSecret': 'kfQgoO6sKkgYcWzUGTOhjy3Y9jHKCXwQNqdeRdw3IQ2SzjREq7',
-        'AccessToken': '2591243785-GWwKbj2PhuIKBmm7ruy7Dj34crD9h0ggFEc0weP',
-        'AccessTokenSecret': 'rIDXTtjAYn07yoA4JxxudZgrzuOAdfLiWiaGowHKAyv2n'
+    Twitter: {
+        ConsumerKey: 'C0kUvowIJyonFKicVQf3NeXvB',
+        ConsumerSecret: 'kfQgoO6sKkgYcWzUGTOhjy3Y9jHKCXwQNqdeRdw3IQ2SzjREq7',
+        AccessToken: '2591243785-GWwKbj2PhuIKBmm7ruy7Dj34crD9h0ggFEc0weP',
+        AccessTokenSecret: 'rIDXTtjAYn07yoA4JxxudZgrzuOAdfLiWiaGowHKAyv2n'
     },
-    'TwitterFollow': {
+    TwitterFollow: {
         // @HitomaruKonpaku
         // '2591243785': {
         //     '422709303376609290': {},
@@ -23,9 +23,9 @@ module.exports = {
             '376294828608061440': {},
         }
     },
-    'KanColle': {
-        'LBAS': 'https://i.imgur.com/0Rxwv8G.png',
-        'AirPower': 'https://i.imgur.com/8DcnG08.png',
-        'Development': 'https://i.imgur.com/ZehPS8X.png'
+    KanColle: {
+        LBAS: 'https://i.imgur.com/0Rxwv8G.png',
+        AirPower: 'https://i.imgur.com/8DcnG08.png',
+        Development: 'https://i.imgur.com/ZehPS8X.png'
     }
 }
