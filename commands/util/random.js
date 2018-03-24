deefc9ad2d589d5d0faa7f6f8b27d105044b00a0
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-03-24 21:35:29 +0700

    Add RNG command

---
+++ commands/util/random.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord.js-commando')
+const Logger = require('../../modules/logger')
+
+module.exports = class RandomCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'rd',
+            group: 'util',
+            memberName: 'rd',
+            description: 'Random number generator BlessRNG',
+            examples: ['rd', 'rd 1 10'],
+            args: [
+                {
+                    key: 'min',
+                    prompt: 'Min value?',
+                    type: 'integer',
+                    default: 1,
+                    min: Number.MIN_SAFE_INTEGER,
+                    max: Number.MAX_SAFE_INTEGER,
+                },
+                {
+                    key: 'max',
+                    prompt: 'Max value?',
+                    type: 'integer',
+                    default: Math.pow(10, 6),
+                    min: Number.MIN_SAFE_INTEGER,
+                    max: Number.MAX_SAFE_INTEGER,
+                },
+            ]
+        })
+    }
+
+    async run(msg, args) {
+        var min = args.min,
+            max = args.max,
+            random = Math.floor(Math.random() * (max - min))
+
+        Logger.log(`Generating random number...`)
+        Logger.debug({ min, max, random })
+        msg.channel.send(`:pray: ${random} :pray:`)
+    }
+}
