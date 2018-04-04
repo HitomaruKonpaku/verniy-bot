a6bd5102cb95bd145ac295039143e61eef42fee1
HitomaruKonpaku <hunteratthenight@gmail.com>
2018-04-04 14:11:42 +0700

    Revert "Add stack trace limit"
    This reverts commit 29d70d25f498c1aa1259633d97a1da33565f2a57.

--- index.js
+++ index.js
@@ -2,10 +2,5 @@
 const ENVIRONMENT = process.env
 require('dotenv').config()
 
-// Global config
-const stackTraceLimit = Number(ENVIRONMENT.STACK_TRACE_LIMIT) || Error.stackTraceLimit
-console.log(`StackTraceLimit: ${stackTraceLimit}`)
-Error.stackTraceLimit = stackTraceLimit
-
 // Start discord client
 require('./modules/discord-client').login()
