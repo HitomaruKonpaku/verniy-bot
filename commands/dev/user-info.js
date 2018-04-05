f8e07ba99c51ccb5575be2e0db48320a11fe0f92
HitomaruKonpaku <hunteratthenight@gmail.com>
2018-04-05 16:56:25 +0700

    Display specific user info

--- commands/dev/user-info.js
+++ commands/dev/user-info.js
@@ -12,21 +12,35 @@ module.exports = class UserInfoCommand extends Command {
 
     async run(msg, args) {
         msg.client.fetchUser(args)
-            .then(data => {
-                var cache = []
-                var json = JSON.stringify(data, function (key, value) {
-                    if (typeof value === 'object' && value !== null) {
-                        if (cache.indexOf(value) !== -1) {
-                            // Circular reference found, discard key
-                            return
-                        }
-                        // Store value in our collection
-                        cache.push(value)
-                    }
-                    return value
-                })
-                cache = null
-                console.log(json)
+            .then(user => {
+                console.log(JSON.stringify({
+                    avatar: user.avatar,
+                    avatarURL: user.avatarURL,
+                    bot: user.bot,
+                    createdAt: user.createdAt,
+                    createdTimestamp: user.createdTimestamp,
+                    defaultAvatarURL: user.defaultAvatarURL,
+                    discriminator: user.discriminator,
+                    displayAvatarURL: user.displayAvatarURL,
+                    id: user.id,
+                    username: user.username,
+                }))
+
+                // var cache = []
+                // var json = JSON.stringify(data, function (key, value) {
+                //     if (typeof value === 'object' && value !== null) {
+                //         if (cache.indexOf(value) !== -1) {
+                //             // Circular reference found, discard key
+                //             return
+                //         }
+                //         // Store value in our collection
+                //         cache.push(value)
+                //     }
+                //     return value
+                // })
+                // cache = null
+                // console.log(json)
+
             })
             .catch(err => {
                 console.trace(err)
