b64b221620f70707c15a60f13d915d70ff012f45
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-04-17 22:52:06 +0700

    Refactor Util

--- src/modules/Util.js
+++ src/modules/Util.js
@@ -1,40 +1,50 @@
 const Logger = require('./Logger')
 
+function broadcastDiscordChannels(discord, channels, message, embed) {
+    let channelList = getDiscordBroadcastChannel(discord, channels)
+    channelList.forEach(v => {
+        v.send(message, embed)
+            .then(() => Logger.log(`Done: ${v.guild.name} > ${v.name}`))
+            .catch(err => Logger.error(err))
+    })
+}
+
+function getDiscordBroadcastChannel(discord, channels) {
+    let set = new Set(channels)
+    let broadcast = discord.channels
+        .filterArray(v =>
+            v.type == 'text' &&
+            set.has(v.id)
+        )
+    return broadcast
+}
+
+function getDiscordCommandWithID(discord, id) {
+    let cmd = discord.registry.commands
+        .filterArray(v => v.name == id)
+    return cmd.length != 0 ? cmd[0] : undefined
+}
+
+function getTwitterFollow(data) {
+    let list = []
+    for (const key in data) {
+        list.push(key)
+    }
+    return list
+}
+
+function getTwitterFollowBroadcast(data) {
+    let list = []
+    for (const key in data.channels) {
+        list.push(key)
+    }
+    return list
+}
+
 module.exports = {
-    broadcastDiscordChannels: (discord, channels, message, embed) => {
-        let channelList = getDiscordBroadcastChannel(discord, channels)
-        channelList.forEach(v => {
-            v.send(message, embed)
-                .then(() => Logger.log(`Done: ${v.guild.name} > ${v.name}`))
-                .catch(err => Logger.error(err))
-        })
-    },
-    getDiscordCommandWithID: (discord, id) => {
-        let cmd = discord.registry.commands
-            .filterArray(v => v.name == id)
-        return cmd.length != 0 ? cmd[0] : undefined
-    },
-    getDiscordBroadcastChannel: (discord, channels) => {
-        let set = new Set(channels)
-        let broadcast = discord.channels
-            .filterArray(v =>
-                v.type == 'text' &&
-                set.has(v.id)
-            )
-        return broadcast
-    },
-    getTwitterFollow: data => {
-        let list = []
-        for (const key in data) {
-            list.push(key)
-        }
-        return list
-    },
-    getTwitterFollowBroadcast: data => {
-        let list = []
-        for (const key in data.follower) {
-            list.push(key)
-        }
-        return list
-    },
+    getDiscordBroadcastChannel: (discord, channels) => getDiscordBroadcastChannel(discord, channels),
+    broadcastDiscordChannels: (discord, channels, message, embed) => broadcastDiscordChannels(discord, channels, message, embed),
+    getDiscordCommandWithID: (discord, id) => getDiscordCommandWithID(discord, id),
+    getTwitterFollow: (data) => getTwitterFollow(data),
+    getTwitterFollowBroadcast: (data) => getTwitterFollowBroadcast(data)
 }
