ac5e987d834e7780cbbe7dbd6555a86ff9ed09e9
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-04-20 01:20:50 +0700

    Add disconnect command

---
+++ src/commands/dev/disconnect.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord.js-commando')
+const Logger = require('../../modules/Logger')
+
+module.exports = class DiscordDisconnectCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'disconnect',
+            group: 'dev',
+            memberName: 'disconnect',
+            description: '',
+        })
+    }
+
+    async run(msg, args) {
+        let isOwner = this.client.isOwner(msg.author)
+        if (!isOwner) {
+            return
+        }
+        Logger.warn('DISCONNECTING')
+        this.client.destroy()
+    }
+}
