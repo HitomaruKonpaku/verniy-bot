b690d4017cf0798ff7f43b194951024ec2113d0a
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-04-21 15:12:59 +0700

    Update restart command to restart all heroku dynos

---
+++ src/commands/dev/heroku-restart.js
@@ -0,0 +1,35 @@
+const { Command } = require('discord.js-commando')
+const Heroku = require('heroku-client')
+const Logger = require('../../modules/Logger')
+
+module.exports = class HerokuRestartCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'restart',
+            group: 'dev',
+            memberName: 'restart',
+            description: '',
+            guarded: true,
+        })
+    }
+
+    async run(msg, args) {
+        let isOwner = this.client.isOwner(msg.author)
+        if (!isOwner) return
+
+        this.client.destroy()
+
+        const key = process.env.HEROKU_API_KEY
+        if (!key) {
+            Logger.warn(`HEROKU_API_KEY not found`)
+            return
+        }
+
+        const heroku = new Heroku({ token: process.env.HEROKU_API_KEY })
+        const appName = 'hito-verniy'
+        heroku
+            .delete(`/apps/${appName}/dynos`)
+            .then(app => Logger.log(`Restarting all dynos`))
+            .catch(err => Logger.error(err))
+    }
+}
