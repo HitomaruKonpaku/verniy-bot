bb6bdcbb08f097737dbe008dc31fafec334e69c0
HitomaruKonpaku <hunteratthenight@gmail.com>
2018-05-14 16:28:50 +0700

    Add ava command to get user ava

---
+++ src/commands/util/user-ava.js
@@ -0,0 +1,28 @@
+const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js')
+const Logger = require('../../modules/Logger')
+
+module.exports = class UserAvaCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ava',
+            group: 'util',
+            memberName: 'ava',
+            description: 'Check user ava',
+            args: [
+                {
+                    key: 'user',
+                    prompt: 'User?',
+                    type: 'user',
+                    wait: 10,
+                },
+            ]
+        })
+    }
+
+    async run(msg, args) {
+        msg.channel
+            .send(`<${args.user.avatarURL}>`)
+            .catch(err => Logger.error(err))
+    }
+}
