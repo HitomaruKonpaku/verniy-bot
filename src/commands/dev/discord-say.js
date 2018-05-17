e90e877fac37269d1218f849ed20fb1bec5bea31
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2018-05-17 23:26:26 +0700

    Add command say

---
+++ src/commands/dev/discord-say.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando')
+const Logger = require('../../modules/Logger')
+
+module.exports = class DiscordSayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'say',
+            group: 'dev',
+            memberName: 'say',
+            description: '',
+            ownerOnly: true,
+            argsType: 'multiple',
+            argsCount: '2',
+        })
+    }
+
+    async run(msg, args) {
+        const regex = /^<*[#@]*\d+>*$/g
+        if (!args || args.length < 2 || !regex.test(args[0])) {
+            return
+        }
+
+        const dest = args[0]
+        const message = args[1]
+        const id = dest.match(/\d+/)[0]
+
+        if (dest.indexOf('@') != -1 || this.client.users.some(v => v.id == id)) {
+            this.client.users.get(id)
+                .send(message)
+                .catch(err => Logger.error(err))
+        } else if (dest.indexOf('#') != -1 || this.client.channels.some(v => v.id == id)) {
+            this.client.channels.get(id)
+                .send(message)
+                .catch(err => Logger.error(err))
+        }
+    }
+}
