a6ab9f71a90649a9abe2f2b87d231e02c131e2d4
HitomaruKonpaku <hunteratthenight@gmail.com>
2018-07-13 22:46:36 +0700

    Refactor settings

--- src/settings.js
+++ src/settings.js
@@ -84,43 +84,60 @@ module.exports = {
             '2591243785': {
                 interval: 10,
                 channels: [
-                    '462085619691290624', // Hitomaru Nuke Zone > other
+                    // Hitomaru Nuke Zone > #other
+                    '462085619691290624',
                 ],
             },
             // @KanColle_STAFF
             '294025417': {
                 interval: 5,
                 channels: [
-                    '462085551734915074', // Hitomaru Nuke Zone > kancolle_staff
-                    '365111136095305730', // Sad Panda > organization
-                    '441955702781771806', // VN KC > dev-tweet
-                    '442409008788275200', // VN KC > chat
-                    '451006611360710656', // Quan nhau cua T > dev-tweet
-                    '368557905718542338', // Dzeso > general
+                    // Hitomaru Nuke Zone > #kancolle_staff
+                    '462085551734915074',
+                    // Sad Panda > #organization
+                    '365111136095305730',
+                    // VN KC > #dev-tweet
+                    '441955702781771806',
+                    // VN KC > #chat
+                    '442409008788275200',
+                    // Quan nhau cua T > #dev-tweet
+                    '451006611360710656',
+                    // Dzeso > #general
+                    '368557905718542338',
                 ],
                 channelsAsUser: [
-                    '425302689887289344', // KC Discord > kc-only
-                    '449024082529353739', // KanColle Wikia > kancolle-chat
-                    '165107321561808896', // KanColle Wiki > kancolle
-                    '334800225191329792', // KC3 > kancolle
-                    '345299518012653570', // lolis > boatsluts-kc
-                    '98922706317103104', // Daxyn > kancolle
+                    // KC Discord > #kc-only
+                    '425302689887289344',
+                    // KanColle Wikia > #kancolle-chat
+                    '449024082529353739',
+                    // KanColle Wiki > #kancolle
+                    '165107321561808896',
+                    // KC3 > #kancolle
+                    '334800225191329792',
+                    // lolis > #boatsluts-kc
+                    '345299518012653570',
+                    // Daxyn > #kancolle
+                    '98922706317103104',
                 ],
             },
             // @GirlsFrontline
             '3247835011': {
                 interval: 60,
                 channels: [
-                    '458855845669634049', // Sad Panda > gf-tweet
-                    '457599318996811776', // lolis > gf-news-feed
+                    // Sad Panda > #gf-tweet
+                    '458855845669634049',
+                    // lolis > #gf-news-feed
+                    '457599318996811776',
                 ],
             },
             // @GirlsFrontlineE
             '958921874523607040': {
                 interval: 60,
                 channels: [
-                    '458855845669634049', // Sad Panda > gf-tweet
-                    '457599318996811776', // lolis > gf-news-feed
+                    // Sad Panda > #gf-tweet
+                    '458855845669634049',
+                    // lolis > #gf-news-feed
+                    '457599318996811776',
                 ],
             },
         },
@@ -137,10 +154,14 @@ module.exports = {
     },
     Cron: {
         KanColle: [
-            '422709303376609290', // Hitomaru Nuke Zone > bot-verniy
-            '442409008788275200', // VN KC > chat
-            '376294828608061440', // Quan nhau cua T > kc-viet
-            '421681074565939201', // Quan nhau cua T > kc-eng
+            // Hitomaru Nuke Zone > #bot-verniy
+            '422709303376609290',
+            // VN KC > #chat
+            '442409008788275200',
+            // Quan nhau cua T > #kc-viet
+            '376294828608061440',
+            // Quan nhau cua T > #kc-eng
+            '421681074565939201',
         ],
     },
 }
