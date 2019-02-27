b50092cb65568c32c78358020c467481575b15e1
HitomaruKonpaku <hunteratthenight@gmail.com>
2019-02-27 23:16:47 +0700

    Send error trace via Discord webhook

--- src/module/Logger.js
+++ src/module/Logger.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const { WebhookClient } = require('discord.js')
+
 module.exports = {
   log: msg => {
     console.log(`INFO ${msg}`)
@@ -12,9 +15,30 @@ module.exports = {
   error: err => {
     const msg = err.message ? err.message : err
     console.log(`ERROR ${msg}`)
+    if (err.stack) {
+      sendDiscordWebhook(err.stack)
+    }
     if (['ECONNRESET'].some(v => msg.includes(v))) {
       return
     }
     console.trace(err)
+  }
+}
+
+async function sendDiscordWebhook(msg) {
+  const whUrl = process.env.APP_NOTIFICATION_DISCORD_WEBHOOK
+  if (!whUrl) return
+  const whData = whUrl.split('/')
+  if (whData.length != 7) return
+  const whId = whData[5]
+  const whToken = whData[6]
+  const whClient = new WebhookClient(whId, whToken)
+  try {
+    const codeBlock = '```'
+    const message = [codeBlock, msg, [...codeBlock].reverse().join('')].join('\n')
+    await whClient.send(message)
+  }
+  catch (err) {
+    console.log(err)
   }
 }
