8e3dc076a8d2b4c42eef25f48456d2f0f634e861
HitomaruKonpaku <hunteratthenight@gmail.com>
2019-03-06 22:39:43 +0700

    Update role member command resp

--- src/command/guild/role-members.js
+++ src/command/guild/role-members.js
@@ -21,13 +21,29 @@ module.exports = class RoleMembersCommand extends Command {
 
   async run(msg, args) {
     const role = args.role
-    const members = role.members
-    const message = [
-      `Found **${members.size}** members with role **${role.name}**`,
-      '```',
-      members.map(v => `${v.displayName}`).join('\n'),
-      '```'
-    ].join('\n')
-    return msg.say(message)
+    const members = role.members.array()
+    const msgMaxChar = 2000
+    const msgHead = `Found **${members.length}** members with role **${role.name}**`
+    const msgBlock = '```'
+    const msgList = []
+    const msgBuilder = [msgHead, msgBlock]
+    // Generate multi messages
+    members.forEach(v => {
+      const name = v.user.tag
+      const tmp = msgBuilder.concat(name, msgBlock).join('\n')
+      if (tmp.length <= msgMaxChar) {
+        msgBuilder.push(name)
+        return
+      }
+      msgBuilder.push(msgBlock)
+      msgList.push(msgBuilder.join('\n'))
+      msgBuilder.splice(0, msgBuilder.length)
+      msgBuilder.push(msgBlock, name)
+    })
+    // Append to last message
+    msgBuilder.push(msgBlock)
+    msgList.push(msgBuilder.join('\n'))
+    // Send
+    msgList.forEach(async v => msg.direct(v))
   }
 }
