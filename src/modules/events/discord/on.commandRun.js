57eea4b8ae053384c2985585e33cb1f4e24d62f6
HitomaruKonpaku <hunteratthenight@gmail.com>
2019-10-11 17:30:05 +0700

    Update command reaction permission

--- src/modules/events/discord/on.commandRun.js
+++ src/modules/events/discord/on.commandRun.js
@@ -1,15 +1,24 @@
 const logger = require('log4js').getLogger('DiscordCommand')
+const { Permissions } = require('discord.js')
 
 module.exports = async function(command, promise, message) {
-  const reply = await promise
-  if (!reply || Array.isArray(reply)) {
+  if (!message.channel.permissionsFor(this.user).has(Permissions.FLAGS.ADD_REACTIONS)) return
+
+  const res = await promise
+  if (!res) {
     return
   }
+
+  if (Array.isArray(res)) {
+    return
+  }
+
+  const reply = res
   try {
     await reply.react('❌')
     reply
       .awaitReactions(
-        (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id,
+        (reaction, user) => reaction.emoji.name === '❌' && (this.isOwner(user) || user.id === message.author.id),
         { max: 1, time: 15 * 1000, errors: ['time'] }
       )
       .then(() => reply.delete())
