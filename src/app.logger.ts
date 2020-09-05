999ba7c6d9c28efe523b946e63d3311044f5d194
HitomaruKonpaku <hunteratthenight@gmail.com>
2020-09-05 13:48:23 +0700

    Add custom logger

---
+++ src/app.logger.ts
@@ -0,0 +1,3 @@
+import { Logger } from '@nestjs/common'
+
+export class AppLogger extends Logger { }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core'
+import { AppLogger } from './app.logger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    // { logger: console },
+    { logger: new AppLogger() },
   )
 
   await app.listen(3000)
