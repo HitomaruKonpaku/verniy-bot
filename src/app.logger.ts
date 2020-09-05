4051da04850b506c2cde003cccc0c1c108be8944
HitomaruKonpaku <hunteratthenight@gmail.com>
2020-09-05 15:45:57 +0700

    Update custom logger

--- src/app.logger.ts
+++ src/app.logger.ts
@@ -1,3 +1,52 @@
 import { Logger } from '@nestjs/common'
 
-export class AppLogger extends Logger { }
+export class AppLogger extends Logger {
+  error(message: any, trace?: string, context?: string): void {
+    const level = 'error'
+    const msg =
+      [
+        this.getMessage(level, message, context),
+        trace,
+      ].filter(v => v).join('\n')
+    console.log(msg)
+  }
+
+  warn(message: any, context?: string): void {
+    const level = 'warn'
+    const msg = this.getMessage(level, message, context)
+    console.log(msg)
+  }
+
+  log(message: any, context?: string): void {
+    const level = 'log'
+    const msg = this.getMessage(level, message, context)
+    console.log(msg)
+  }
+
+  debug(message: any, context?: string): void {
+    const level = 'debug'
+    const msg = this.getMessage(level, message, context)
+    console.log(msg)
+  }
+
+  private getMessage(level: string, message: any, context?: string) {
+    const date = new Date()
+    date.setMinutes(date.getMinutes() - date.getTimezoneOffset())
+    const time = date.toISOString().replace(/[TZ]/g, ' ').trim()
+    const lvl = this.getLevel(level)
+    const msg = [
+      [
+        time,
+        lvl,
+        context,
+      ].filter(v => v).map(v => `[${v}]`).join(''),
+      message,
+    ].join(' ')
+    return msg
+  }
+
+  private getLevel(level: string) {
+    const s = level.toUpperCase().padEnd(5, ' ')
+    return s
+  }
+}
