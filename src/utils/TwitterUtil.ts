7b50792ee43e63e4596c03b7430daa893647a702
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-03-12 11:49:13 +0700

    Fix discord embed

--- src/utils/TwitterUtil.ts
+++ src/utils/TwitterUtil.ts
@@ -15,7 +15,18 @@ export class TwitterUtil {
   }
 
   public static getTweetUrl(tweet: Twit.Twitter.Status): string {
-    return `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+    // Fix retweet not show embed by discord
+    const host = tweet.retweeted_status && tweet.text?.startsWith?.('RT @')
+      ? 'fxtwitter.com'
+      : 'twitter.com'
+    const url = `https://${host}/${tweet.user.screen_name}/status/${tweet.id_str}`
+    return url
+  }
+
+  public static getInReplyToTweetUrl(tweet: Twit.Twitter.Status): string {
+    const host = 'twitter.com'
+    const url = `https://${host}/${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}`
+    return url
   }
 
   public static getProfileDefaultInterval(): number {
