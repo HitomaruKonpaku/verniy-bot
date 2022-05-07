1c538141cddf00ef3d45822ba0842490130e9891
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-05-07 15:30:36 +0700

    Add index logger

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import 'dotenv/config'
 import { AppModule } from './app.module'
 import { AppService } from './app.service'
+import { logger } from './logger'
 
 async function bootstrap() {
+  logger.info(Array(50).fill('=').join(''))
   // process.env.NO_COLOR = '1'
   const app = await NestFactory.createApplicationContext(AppModule)
   const appService = app.get(AppService)
