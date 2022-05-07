4b300f151a2ee075dfb684c0ba31eb413d6146d5
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-05-07 15:23:31 +0700

    Update deploy commands

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -6,11 +6,17 @@ import 'dotenv/config'
 import { DISCORD_COMMANDS } from './modules/discord/constants/discord-command.constant'
 
 const updateGuildCommands = true
-const updateGlobalCommands = false
+const updateGlobalCommands = !true
 
 const commands = DISCORD_COMMANDS
   .map((v) => v.command)
   .map((command) => command.toJSON())
+
+const guildCommands = Array.from(commands)
+const globalCommands = Array.from(commands)
+
+// guildCommands.length = 0
+globalCommands.length = 0
 
 async function main() {
   const token = process.env.DISCORD_TOKEN
@@ -29,7 +35,7 @@ async function main() {
 
   if (updateGuildCommands && appId && guildId) {
     try {
-      await rest.put(Routes.applicationGuildCommands(appId, guildId), { body: commands })
+      await rest.put(Routes.applicationGuildCommands(appId, guildId), { body: guildCommands })
       console.log('Successfully registered application guild commands')
     } catch (error) {
       console.error(error)
@@ -39,7 +45,7 @@ async function main() {
 
   if (updateGlobalCommands && appId) {
     try {
-      await rest.put(Routes.applicationCommands(appId), { body: commands })
+      await rest.put(Routes.applicationCommands(appId), { body: globalCommands })
       console.log('Successfully registered application commands')
     } catch (error) {
       console.error(error)
