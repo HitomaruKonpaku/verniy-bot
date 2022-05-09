e2c095cb86df1b667c200b12f962eecd0a26c31f
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-05-09 23:43:32 +0700

    Auto active debug console log

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import 'dotenv/config'
 import { AppModule } from './app.module'
 import { AppService } from './app.service'
-import { logger } from './logger'
+import { logger, toggleDebugConsole } from './logger'
 
 async function bootstrap() {
   logger.info(Array(50).fill('=').join(''))
+  if (!process.env.NODE_ENV) {
+    toggleDebugConsole()
+  }
   // process.env.NO_COLOR = '1'
   const app = await NestFactory.createApplicationContext(AppModule)
   const appService = app.get(AppService)
