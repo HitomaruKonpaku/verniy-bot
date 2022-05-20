df11d336e2c5b66bec00fca13a675f85efbdd718
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-05-20 12:44:39 +0700

    Reduce log payload

--- src/modules/twitter/services/twitter-api.service.ts
+++ src/modules/twitter/services/twitter-api.service.ts
@@ -11,140 +11,140 @@ import { TwitterClientService } from './twitter-client.service'
 export class TwitterApiService {
   private readonly logger = baseLogger.child({ context: TwitterApiService.name })
 
- private readonly SPACE_LOOKUP_OPTIONS:Partial<SpaceV2CreatorLookupParams> = {
-   expansions: ['creator_id', 'host_ids', 'speaker_ids'],
-   'space.fields': ['id', 'created_at', 'updated_at', 'creator_id', 'state', 'is_ticketed', 'scheduled_start', 'started_at', 'ended_at', 'lang', 'title', 'host_ids', 'speaker_ids'],
- }
+  private readonly SPACE_LOOKUP_OPTIONS: Partial<SpaceV2CreatorLookupParams> = {
+    expansions: ['creator_id', 'host_ids', 'speaker_ids'],
+    'space.fields': ['id', 'created_at', 'updated_at', 'creator_id', 'state', 'is_ticketed', 'scheduled_start', 'started_at', 'ended_at', 'lang', 'title', 'host_ids', 'speaker_ids'],
+  }
 
- constructor(
+  constructor(
     @Inject(TwitterClientService)
     private readonly twitterClientService: TwitterClientService,
- ) { }
+  ) { }
 
- private get client() {
-   return this.twitterClientService.roClient
- }
+  private get client() {
+    return this.twitterClientService.roClient
+  }
 
- public async getUserById(id: string): Promise<UserV1> {
-   const requestId = randomUUID()
-   try {
-     this.logger.debug('--> getUserById', { requestId, id })
-     const user = await this.client.v1.user({ user_id: id })
-     this.logger.debug('<-- getUserById', { requestId })
-     return user
-   } catch (error) {
-     this.logger.error(`getUserById: ${error.message}`, { requestId, id })
-     throw error
-   }
- }
+  public async getUserById(id: string): Promise<UserV1> {
+    const requestId = randomUUID()
+    try {
+      this.logger.debug('--> getUserById', { requestId, id })
+      const user = await this.client.v1.user({ user_id: id })
+      this.logger.debug('<-- getUserById', { requestId })
+      return user
+    } catch (error) {
+      this.logger.error(`getUserById: ${error.message}`, { requestId, id })
+      throw error
+    }
+  }
 
- public async getUserByUsername(username: string): Promise<UserV1> {
-   const requestId = randomUUID()
-   try {
-     this.logger.debug('--> getUserByUsername', { requestId, username })
-     const user = await this.client.v1.user({ screen_name: username })
-     this.logger.debug('<-- getUserByUsername', { requestId })
-     return user
-   } catch (error) {
-     this.logger.error(`getUserByUsername: ${error.message}`, { requestId, username })
-     throw error
-   }
- }
+  public async getUserByUsername(username: string): Promise<UserV1> {
+    const requestId = randomUUID()
+    try {
+      this.logger.debug('--> getUserByUsername', { requestId, username })
+      const user = await this.client.v1.user({ screen_name: username })
+      this.logger.debug('<-- getUserByUsername', { requestId })
+      return user
+    } catch (error) {
+      this.logger.error(`getUserByUsername: ${error.message}`, { requestId, username })
+      throw error
+    }
+  }
 
- public async getAllUsersByUserIds(userIds: string[]): Promise<UserV1[]> {
-   const chunks = Utils.splitArrayIntoChunk(userIds, TWITTER_API_LIST_SIZE)
-   const results = await Promise.allSettled(chunks.map((v) => this.getUsersByUserIds(v)))
-   const users = results.filter((v) => v.status === 'fulfilled').flatMap((v: any) => v.value)
-   return users
- }
+  public async getAllUsersByUserIds(userIds: string[]): Promise<UserV1[]> {
+    const chunks = Utils.splitArrayIntoChunk(userIds, TWITTER_API_LIST_SIZE)
+    const results = await Promise.allSettled(chunks.map((v) => this.getUsersByUserIds(v)))
+    const users = results.filter((v) => v.status === 'fulfilled').flatMap((v: any) => v.value)
+    return users
+  }
 
- public async getUsersByUserIds(userIds: string[]): Promise<UserV1[]> {
-   const requestId = randomUUID()
-   try {
-     this.logger.debug('--> getUsersByUserIds', { requestId, userCount: userIds.length, userIds })
-     const users = await this.client.v1.users({ user_id: userIds })
-     this.logger.debug('<-- getUsersByUserIds', { requestId })
-     return users
-   } catch (error) {
-     this.logger.error(`getUsersByUserIds: ${error.message}`, { requestId })
-     throw error
-   }
- }
+  public async getUsersByUserIds(userIds: string[]): Promise<UserV1[]> {
+    const requestId = randomUUID()
+    try {
+      this.logger.debug('--> getUsersByUserIds', { requestId, userCount: userIds.length })
+      const users = await this.client.v1.users({ user_id: userIds })
+      this.logger.debug('<-- getUsersByUserIds', { requestId })
+      return users
+    } catch (error) {
+      this.logger.error(`getUsersByUserIds: ${error.message}`, { requestId })
+      throw error
+    }
+  }
 
- public async getAllUsersByUsernames(usernames: string[]): Promise<UserV1[]> {
-   const chunks = Utils.splitArrayIntoChunk(usernames, TWITTER_API_LIST_SIZE)
-   const results = await Promise.allSettled(chunks.map((v) => this.getUsersByUsernames(v)))
-   const users = results.filter((v) => v.status === 'fulfilled').flatMap((v: any) => v.value)
-   return users
- }
+  public async getAllUsersByUsernames(usernames: string[]): Promise<UserV1[]> {
+    const chunks = Utils.splitArrayIntoChunk(usernames, TWITTER_API_LIST_SIZE)
+    const results = await Promise.allSettled(chunks.map((v) => this.getUsersByUsernames(v)))
+    const users = results.filter((v) => v.status === 'fulfilled').flatMap((v: any) => v.value)
+    return users
+  }
 
- public async getUsersByUsernames(usernames: string[]): Promise<UserV1[]> {
-   const requestId = randomUUID()
-   try {
-     this.logger.debug('--> getUsersByUsernames', { requestId, userCount: usernames.length, usernames })
-     const users = await this.client.v1.users({ screen_name: usernames })
-     this.logger.debug('<-- getUsersByUsernames', { requestId })
-     return users
-   } catch (error) {
-     this.logger.error(`getUsersByUsernames: ${error.message}`, { requestId })
-     throw error
-   }
- }
+  public async getUsersByUsernames(usernames: string[]): Promise<UserV1[]> {
+    const requestId = randomUUID()
+    try {
+      this.logger.debug('--> getUsersByUsernames', { requestId, userCount: usernames.length })
+      const users = await this.client.v1.users({ screen_name: usernames })
+      this.logger.debug('<-- getUsersByUsernames', { requestId })
+      return users
+    } catch (error) {
+      this.logger.error(`getUsersByUsernames: ${error.message}`, { requestId })
+      throw error
+    }
+  }
 
- public async getSpaceById(id: string) {
-   const requestId = randomUUID()
-   try {
-     const result = await twitterSpaceLimiter.schedule(async () => {
-       this.logger.debug('--> getSpaceById', { requestId, id })
-       const response = await this.client.v2.space(
-         id,
-         this.SPACE_LOOKUP_OPTIONS,
-       )
-       this.logger.debug('<-- getSpaceById', { requestId })
-       return Promise.resolve(response)
-     })
-     return result
-   } catch (error) {
-     this.logger.error(`getSpaceById: ${error.message}`, { requestId, id })
-     throw error
-   }
- }
+  public async getSpaceById(id: string) {
+    const requestId = randomUUID()
+    try {
+      const result = await twitterSpaceLimiter.schedule(async () => {
+        this.logger.debug('--> getSpaceById', { requestId, id })
+        const response = await this.client.v2.space(
+          id,
+          this.SPACE_LOOKUP_OPTIONS,
+        )
+        this.logger.debug('<-- getSpaceById', { requestId })
+        return Promise.resolve(response)
+      })
+      return result
+    } catch (error) {
+      this.logger.error(`getSpaceById: ${error.message}`, { requestId, id })
+      throw error
+    }
+  }
 
- public async getSpacesByIds(ids: string[]) {
-   const requestId = randomUUID()
-   try {
-     const result = await twitterSpacesByIdsLimiter.schedule(async () => {
-       this.logger.debug('--> getSpacesByIds', { requestId, idCount: ids.length, ids })
-       const response = await this.client.v2.spaces(
-         ids,
-         this.SPACE_LOOKUP_OPTIONS,
-       )
-       this.logger.debug('<-- getSpacesByIds', { requestId })
-       return Promise.resolve(response)
-     })
-     return result
-   } catch (error) {
-     this.logger.error(`getSpacesByIds: ${error.message}`, { requestId })
-     throw error
-   }
- }
+  public async getSpacesByIds(ids: string[]) {
+    const requestId = randomUUID()
+    try {
+      const result = await twitterSpacesByIdsLimiter.schedule(async () => {
+        this.logger.debug('--> getSpacesByIds', { requestId, idCount: ids.length, ids })
+        const response = await this.client.v2.spaces(
+          ids,
+          this.SPACE_LOOKUP_OPTIONS,
+        )
+        this.logger.debug('<-- getSpacesByIds', { requestId })
+        return Promise.resolve(response)
+      })
+      return result
+    } catch (error) {
+      this.logger.error(`getSpacesByIds: ${error.message}`, { requestId })
+      throw error
+    }
+  }
 
- public async getSpacesByCreatorIds(userIds: string[]) {
-   const requestId = randomUUID()
-   try {
-     const result = await twitterSpacesByCreatorIdsLimiter.schedule(async () => {
-       this.logger.debug('--> getSpacesByCreatorIds', { requestId, userCount: userIds.length, userIds })
-       const response = await this.client.v2.spacesByCreators(
-         userIds,
-         this.SPACE_LOOKUP_OPTIONS,
-       )
-       this.logger.debug('<-- getSpacesByCreatorIds', { requestId })
-       return Promise.resolve(response)
-     })
-     return result
-   } catch (error) {
-     this.logger.error(`getSpacesByCreatorIds: ${error.message}`, { requestId })
-     throw error
-   }
- }
+  public async getSpacesByCreatorIds(userIds: string[]) {
+    const requestId = randomUUID()
+    try {
+      const result = await twitterSpacesByCreatorIdsLimiter.schedule(async () => {
+        this.logger.debug('--> getSpacesByCreatorIds', { requestId, userCount: userIds.length })
+        const response = await this.client.v2.spacesByCreators(
+          userIds,
+          this.SPACE_LOOKUP_OPTIONS,
+        )
+        this.logger.debug('<-- getSpacesByCreatorIds', { requestId })
+        return Promise.resolve(response)
+      })
+      return result
+    } catch (error) {
+      this.logger.error(`getSpacesByCreatorIds: ${error.message}`, { requestId })
+      throw error
+    }
+  }
 }
