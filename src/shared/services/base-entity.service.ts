477222059dc7c73b1bcfd97346891a38e07dbcb3
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-05-25 20:44:30 +0700

    Update naming

--- src/shared/services/base-entity.service.ts
+++ src/shared/services/base-entity.service.ts
@@ -4,15 +4,15 @@ export abstract class BaseEntityService<T> {
   public readonly repository: Repository<T>
 
   public async getOneById(id: string) {
-    const record = await this.repository
+    const result = await this.repository
       .createQueryBuilder()
       .andWhere('id = :id', { id })
       .getOne()
-    return record
+    return result
   }
 
   public async save(data: T): Promise<T> {
-    const record = await this.repository.save(data)
-    return record
+    const result = await this.repository.save(data)
+    return result
   }
 }
