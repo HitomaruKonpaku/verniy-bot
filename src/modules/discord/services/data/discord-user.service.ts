d0cf338172bd203ebf296dd66d06e8985231cf61
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-05-25 22:26:58 +0700

    Fix error

--- src/modules/discord/services/data/discord-user.service.ts
+++ src/modules/discord/services/data/discord-user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { BaseEntityService } from '../../../../shared/services/base-entity.service'
 import { DiscordUser } from '../../models/discord-user.entity'
@@ -6,6 +7,7 @@ import { DiscordUser } from '../../models/discord-user.entity'
 @Injectable()
 export class DiscordUserService extends BaseEntityService<DiscordUser> {
   constructor(
+    @InjectRepository(DiscordUser)
     public readonly repository: Repository<DiscordUser>,
   ) {
     super()
