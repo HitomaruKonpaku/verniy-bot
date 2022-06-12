c6c1fdf3adda9c60e83d581d132a33933a9892ce
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-06-12 16:12:00 +0700

    Refactor providers

--- src/modules/discord/constants/discord-command.constant.ts
+++ src/modules/discord/constants/discord-command.constant.ts
@@ -1,5 +1,17 @@
 import { GetCommand } from '../commands/get/get.command'
+import { TrackAddTiktokVideoCommand } from '../commands/track/track-add/track-add-tiktok-video.command'
+import { TrackAddTwitCastingLiveCommand } from '../commands/track/track-add/track-add-twitcasting-live.command'
+import { TrackAddTwitchUserStreamCommand } from '../commands/track/track-add/track-add-twitch-user-stream.command'
+import { TrackAddTwitterProfileCommand } from '../commands/track/track-add/track-add-twitter-profile.command'
+import { TrackAddTwitterSpaceCommand } from '../commands/track/track-add/track-add-twitter-space.command'
+import { TrackAddTwitterTweetCommand } from '../commands/track/track-add/track-add-twitter-tweet.command'
 import { TrackAddCommand } from '../commands/track/track-add/track-add.command'
+import { TrackRemoveTiktokVideoCommand } from '../commands/track/track-remove/track-remove-tiktok-video.command'
+import { TrackRemoveTwitCastingLiveCommand } from '../commands/track/track-remove/track-remove-twitcasting-live.command'
+import { TrackRemoveTwitchUserStreamCommand } from '../commands/track/track-remove/track-remove-twitch-user-stream.command'
+import { TrackRemoveTwitterProfileCommand } from '../commands/track/track-remove/track-remove-twitter-profile.command'
+import { TrackRemoveTwitterSpaceCommand } from '../commands/track/track-remove/track-remove-twitter-space.command'
+import { TrackRemoveTwitterTweetCommand } from '../commands/track/track-remove/track-remove-twitter-tweet.command'
 import { TrackRemoveCommand } from '../commands/track/track-remove/track-remove.command'
 
 export const DISCORD_APP_COMMANDS = [
@@ -11,11 +23,34 @@ export const DISCORD_APP_COMMANDS = [
 export const DISCORD_GUILD_COMMANDS = [
   // TrackAddCommand,
   // TrackRemoveCommand,
-  GetCommand,
+  // GetCommand,
 ]
 
 export const DISCORD_GLOBAL_COMMANDS = [
   // TrackAddCommand,
   // TrackRemoveCommand,
   // GetCommand,
+]
+
+export const DISCORD_ALL_COMMANDS = [
+  // track-add
+  TrackAddCommand,
+  TrackAddTwitterTweetCommand,
+  TrackAddTwitterProfileCommand,
+  TrackAddTwitterSpaceCommand,
+  TrackAddTwitCastingLiveCommand,
+  TrackAddTwitchUserStreamCommand,
+  TrackAddTiktokVideoCommand,
+
+  // track-remove
+  TrackRemoveCommand,
+  TrackRemoveTwitterTweetCommand,
+  TrackRemoveTwitterProfileCommand,
+  TrackRemoveTwitterSpaceCommand,
+  TrackRemoveTwitCastingLiveCommand,
+  TrackRemoveTwitchUserStreamCommand,
+  TrackRemoveTiktokVideoCommand,
+
+  // get
+  GetCommand,
 ]
--- src/modules/discord/discord.module.ts
+++ src/modules/discord/discord.module.ts
@@ -8,21 +8,7 @@ import { TrackModule } from '../track/track.module'
 import { TwitCastingModule } from '../twitcasting/twitcasting.module'
 import { TwitchModule } from '../twitch/twitch.module'
 import { TwitterModule } from '../twitter/twitter.module'
-import { GetCommand } from './commands/get/get.command'
-import { TrackAddTiktokVideoCommand } from './commands/track/track-add/track-add-tiktok-video.command'
-import { TrackAddTwitCastingLiveCommand } from './commands/track/track-add/track-add-twitcasting-live.command'
-import { TrackAddTwitchUserStreamCommand } from './commands/track/track-add/track-add-twitch-user-stream.command'
-import { TrackAddTwitterProfileCommand } from './commands/track/track-add/track-add-twitter-profile.command'
-import { TrackAddTwitterSpaceCommand } from './commands/track/track-add/track-add-twitter-space.command'
-import { TrackAddTwitterTweetCommand } from './commands/track/track-add/track-add-twitter-tweet.command'
-import { TrackAddCommand } from './commands/track/track-add/track-add.command'
-import { TrackRemoveTiktokVideoCommand } from './commands/track/track-remove/track-remove-tiktok-video.command'
-import { TrackRemoveTwitCastingLiveCommand } from './commands/track/track-remove/track-remove-twitcasting-live.command'
-import { TrackRemoveTwitchUserStreamCommand } from './commands/track/track-remove/track-remove-twitch-user-stream.command'
-import { TrackRemoveTwitterProfileCommand } from './commands/track/track-remove/track-remove-twitter-profile.command'
-import { TrackRemoveTwitterSpaceCommand } from './commands/track/track-remove/track-remove-twitter-space.command'
-import { TrackRemoveTwitterTweetCommand } from './commands/track/track-remove/track-remove-twitter-tweet.command'
-import { TrackRemoveCommand } from './commands/track/track-remove/track-remove.command'
+import { DISCORD_ALL_COMMANDS } from './constants/discord-command.constant'
 import { DiscordChannel } from './models/discord-channel.entity'
 import { DiscordGuild } from './models/discord-guild.entity'
 import { DiscordMessage } from './models/discord-message.entity'
@@ -60,24 +46,7 @@ import { DiscordService } from './services/discord.service'
     DiscordGuildService,
     DiscordChannelService,
     DiscordMessageService,
-    //
-    TrackAddCommand,
-    TrackAddTwitterTweetCommand,
-    TrackAddTwitterProfileCommand,
-    TrackAddTwitterSpaceCommand,
-    TrackAddTwitCastingLiveCommand,
-    TrackAddTwitchUserStreamCommand,
-    TrackAddTiktokVideoCommand,
-    //
-    TrackRemoveCommand,
-    TrackRemoveTwitterTweetCommand,
-    TrackRemoveTwitterProfileCommand,
-    TrackRemoveTwitterSpaceCommand,
-    TrackRemoveTwitCastingLiveCommand,
-    TrackRemoveTwitchUserStreamCommand,
-    TrackRemoveTiktokVideoCommand,
-    //
-    GetCommand,
+    ...DISCORD_ALL_COMMANDS,
   ],
   exports: [
     DiscordService,
