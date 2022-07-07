10b40f3488f7145f91c438f959a9effcabc71460
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-07-07 22:41:53 +0700

    Update array utils

--- src/utils/array.utils.ts
+++ src/utils/array.utils.ts
@@ -4,4 +4,16 @@ export class ArrayUtils {
     return [...Array(Math.ceil(arr.length / chunkSize))]
       .map(() => arr.splice(0, chunkSize))
   }
+
+  public static intersection<T>(arr1: T[], arr2: T[]): T[] {
+    return arr1.filter((v) => arr2.includes(v))
+  }
+
+  public static union<T>(arr1: T[], arr2: T[]): T[] {
+    return [...new Set([...arr1, ...arr2])]
+  }
+
+  public static difference<T>(arr1: T[], arr2: T[]): T[] {
+    return arr1.filter((v) => !arr2.includes(v))
+  }
 }
