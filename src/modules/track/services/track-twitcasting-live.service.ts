e67629ba105e54f1106fad8c5161fd48e3ddd7cf
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-07-16 04:51:37 +0700

    Refactor TwitCasting query

--- src/modules/track/services/track-twitcasting-live.service.ts
+++ src/modules/track/services/track-twitcasting-live.service.ts
@@ -14,27 +14,31 @@ export class TrackTwitCastingLiveService extends TrackBaseService<TrackTwitCasti
   }
 
   public async getUserIdsForInit(): Promise<string[]> {
-    const records = await this.repository
-      .createQueryBuilder('t')
-      .select('t.user_id')
-      .distinct()
-      .leftJoin('twitcasting_user', 'u', 'u.id = t.user_id')
-      .andWhere('t.is_active = TRUE')
-      .andWhere('u.id ISNULL')
-      .getRawMany()
+    const query = `
+SELECT DISTINCT(user_id)
+FROM track AS t
+  LEFT JOIN twitcasting_user AS u ON u.id = t.user_id
+WHERE t.type = 'twitcasting_live'
+  AND t.is_active = TRUE
+  AND u.id ISNULL
+ORDER BY t.created_at
+    `
+    const records = await this.repository.query(query)
     const ids = records.map((v) => v.user_id)
     return ids
   }
 
   public async getScreenIdsForLiveCheck(): Promise<string[]> {
-    const records = await this.repository
-      .createQueryBuilder('t')
-      .select('u.screen_id')
-      .distinct()
-      .leftJoin('twitcasting_user', 'u', 'u.id = t.user_id')
-      .andWhere('t.is_active = TRUE')
-      .andWhere('u.id NOTNULL')
-      .getRawMany()
+    const query = `
+SELECT DISTINCT(screen_id)
+FROM track AS t
+  LEFT JOIN twitcasting_user AS u ON u.id = t.user_id
+WHERE t.type = 'twitcasting_live'
+  AND t.is_active = TRUE
+  AND u.id NOTNULL
+ORDER BY t.created_at
+    `
+    const records = await this.repository.query(query)
     const ids = records.map((v) => v.screen_id)
     return ids
   }
