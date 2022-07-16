78b0162c97cf30bd9d5708ffd56d120be8a10d17
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-07-17 00:55:41 +0700

    Slow down even more

--- src/modules/instagram/instagram.limiter.ts
+++ src/modules/instagram/instagram.limiter.ts
@@ -2,10 +2,10 @@ import Bottleneck from 'bottleneck'
 
 export const instagramUserLimiter = new Bottleneck({
   maxConcurrent: 1,
-  minTime: 500,
+  minTime: 2000,
 })
 
 export const instagramUserStoriesLimiter = new Bottleneck({
   maxConcurrent: 1,
-  minTime: 1000,
+  minTime: 2000,
 })
