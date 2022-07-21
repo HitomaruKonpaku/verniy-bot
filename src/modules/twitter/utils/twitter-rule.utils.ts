b3e40b4bd2b16c41cc9daf8c392c9de05a1e6d54
HitomaruKonpaku <hunteratthenight@gmail.com>
2022-07-22 01:11:15 +0700

    Rewrite rule builder

--- src/modules/twitter/utils/twitter-rule.utils.ts
+++ src/modules/twitter/utils/twitter-rule.utils.ts
@@ -1,62 +1,91 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
+
 import { TWITTER_STREAM_RULE_LENGTH } from '../constants/twitter.constant'
 
 export class TwitterRuleUtils {
-  public static calcRuleLength(usernames: string[]): number {
-    return usernames.reduce((pv, cv) => pv + cv.length, 0)
-      + usernames.length * 5 // `from:` length
-      + (usernames.length - 1) * 4 // ` OR ` length
+  private static readonly OPERATORS = {
+    from: 'from:',
+    or: ' OR ',
   }
 
-  public static calcRuleRemainLength(
-    usernames: string[],
-    ruleMaxLength = TWITTER_STREAM_RULE_LENGTH,
-  ): number {
-    return ruleMaxLength - (
-      usernames.reduce((pv, cv) => pv + cv.length, 0)
-      + (usernames.length + 1) * 5// `from:` length
-      + usernames.length * 4 // ` OR ` length
-    )
-  }
-
-  /**
-   * Build rules by usernames while trying to fit as many username as possible into 1 rule
-   */
   public static buildStreamRulesByUsernames(
     usernames: string[],
     ruleMaxLength = TWITTER_STREAM_RULE_LENGTH,
   ): string[] {
-    /**
-     * Length of ` OR from:X`
-     */
-    const minUserRuleThreshold = 10
-    const arr1: string[] = Array.from(usernames)
-      .filter((v) => v)
-      .sort((a, b) => b.length - a.length || a.toLowerCase().localeCompare(b.toLowerCase()))
-    const arr2: string[] = []
-    const arrResult: string[][] = []
+    if (!usernames?.length) {
+      return []
+    }
 
-    while (arr1.length) {
-      arr2.push(arr1.shift())
-      const ruleLength = this.calcRuleLength(arr2)
-      if (ruleLength + minUserRuleThreshold > ruleMaxLength) {
-        arr1.unshift(arr2.pop())
-        const ruleRemainLength = this.calcRuleRemainLength(arr2)
-        const index = arr1.findIndex((v) => v.length <= ruleRemainLength)
-        if (index >= 0) {
-          const username = arr1.splice(index, 1)[0]
-          arr2.push(username)
-        }
-        arrResult.push(Array.from(arr2))
-        arr2.length = 0
-        // eslint-disable-next-line no-continue
-        continue
-      }
-      if (!arr1.length) {
-        arrResult.push(Array.from(arr2))
+    // Ensure valid input
+    usernames = usernames.slice()
+    ruleMaxLength = Math.max(0, ruleMaxLength)
+
+    // Transform to number array
+    const lengths = usernames.map((v) => v.length)
+    const lengthChunks: number[][] = []
+
+    // Generate array with max possible amount of user in single rule
+    while (lengths.length && ruleMaxLength > 0) {
+      const result = this.howSum(ruleMaxLength, lengths)
+      if (result) {
+        // Save current result
+        lengthChunks.push(result)
+        // Remove result from origin array
+        result.forEach((v) => lengths.splice(lengths.indexOf(v), 1))
+      } else {
+        // Reduce rule max length to find best fit for single rule
+        ruleMaxLength--
       }
     }
 
-    const rules = arrResult.map((result) => result.map((v) => `from:${v}`).join(' OR '))
+    // Transform number array back to username array
+    const usernameChunks = lengthChunks.map((chunk) => {
+      const usernameChunk = chunk.map((length) => {
+        const index = usernames.findIndex((v) => v.length === length)
+        const username = usernames.splice(index, 1)[0]
+        return username
+      })
+      return usernameChunk
+    })
+
+    // Build rules
+    const rules = usernameChunks.map((chunk) => chunk.map((v) => this.OPERATORS.from + v).join(this.OPERATORS.or))
+
+    // debugger
+
     return rules
+  }
+
+  /**
+   * howSum memoization
+   *
+   * @see https://www.youtube.com/watch?v=oBt53YbR9Kk&t=5369s
+   */
+  private static howSum(
+    targetSum: number,
+    numbers: number[],
+    memo: Record<number, number[]> = {},
+    stack = 0,
+  ) {
+    if (targetSum in memo) return memo[targetSum]
+    if (targetSum === 0) return []
+    if (targetSum < 0) return null
+
+    numbers = numbers.slice()
+
+    for (let i = 0; i < numbers.length; i++) {
+      const curNumber = numbers[i]
+      const remainNumbers = numbers.filter((_, j) => j > i)
+      const remainder = targetSum - curNumber - this.OPERATORS.from.length - (stack ? this.OPERATORS.or.length : 0)
+      const remainderResult = this.howSum(remainder, remainNumbers, memo, stack + 1)
+      if (remainderResult !== null) {
+        memo[targetSum] = [curNumber, ...remainderResult]
+        return memo[targetSum]
+      }
+    }
+
+    memo[targetSum] = null
+    return memo[targetSum]
   }
 }
