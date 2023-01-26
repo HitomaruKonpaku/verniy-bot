de7fb4d4c34ede8d4401632d7c2ffe0e3015d7fd
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-01-26 13:39:21 +0700

    Add missing playlist_id

--- src/module/youtube/model/youtube-playlist-item.entity.ts
+++ src/module/youtube/model/youtube-playlist-item.entity.ts
@@ -1,12 +1,16 @@
 import { Column, Entity } from 'typeorm'
 import { BaseExternalEntity } from '../../database/model/base-external.entity'
 import { YoutubeChannel } from './youtube-channel.entity'
+import { YoutubePlaylist } from './youtube-playlist.entity'
 import { YoutubeVideo } from './youtube-video.entity'
 
 @Entity('youtube_playlist_item')
 export class YoutubePlaylistItem extends BaseExternalEntity {
   @Column({ name: 'channel_id', type: 'text' })
   channelId: string
+
+  @Column({ name: 'playlist_id', type: 'text' })
+  playlistId: string
 
   @Column({ name: 'video_id', type: 'text' })
   videoId: string
@@ -15,6 +19,8 @@ export class YoutubePlaylistItem extends BaseExternalEntity {
   position?: number
 
   channel?: YoutubeChannel
+
+  playlist?: YoutubePlaylist
 
   video?: YoutubeVideo
 }
