29896feb544b5e11d1995a51a262c1649caef738
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-02-01 15:22:19 +0700

    Update YT util

--- src/module/youtube/util/youtube-video.util.ts
+++ src/module/youtube/util/youtube-video.util.ts
@@ -1,6 +1,6 @@
-export class YoutubeChannelUtil {
+export class YoutubeVideoUtil {
   public static parseId(s: string): string {
-    const pattern = /UC[\w-]+/
+    const pattern = /(?<=v=|\/)[\w-]{11}/
     const value = pattern.exec(s)?.[0] || s
     return value
   }
