7526a9fc530fd135b4d87d4c3d9dca6f99553493
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-02-21 09:56:24 +0700

    Update ignore list

--- src/module/twitter/service/data/twitter-space.service.ts
+++ src/module/twitter/service/data/twitter-space.service.ts
@@ -133,7 +133,10 @@ SELECT su.id
 FROM twitter_space_user AS su
   LEFT JOIN twitter_user AS u ON u.id = su.id
 WHERE u.id ISNULL
-  AND su.id NOT IN('1261213933089652738')
+  AND su.id NOT IN(
+    '1261213933089652738',
+    '1497504422360150020'
+  )
 ORDER BY CAST(su.id AS NUMBER)
     `
     const records = await this.repository.query(query)
