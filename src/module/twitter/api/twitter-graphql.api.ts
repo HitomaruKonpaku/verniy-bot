67eeab7b5dd8fbcb4405a3462724ff347918d6b5
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-04-23 07:08:34 +0700

    Update twiter GQL API

--- src/module/twitter/api/twitter-graphql.api.ts
+++ src/module/twitter/api/twitter-graphql.api.ts
@@ -3,6 +3,56 @@ import { TWITTER_API_URL } from '../constant/twitter.constant'
 import { TwitterAuthGuestTokenHeaders } from '../interface/twitter-api.interface'
 
 export class TwitterGraphqlApi {
+  public static async getUserByRestId(
+    userId: string,
+    headers: TwitterAuthGuestTokenHeaders,
+  ) {
+    const url = 'graphql/GazOglcBvgLigl3ywt6b3Q/UserByRestId'
+    const response = await axios.get(url, {
+      baseURL: TWITTER_API_URL,
+      headers,
+      params: {
+        variables: {
+          userId,
+          withSafetyModeUserFields: true,
+        },
+        features: {
+          blue_business_profile_image_shape_enabled: true,
+          responsive_web_graphql_exclude_directive_enabled: true,
+          verified_phone_label_enabled: false,
+          responsive_web_graphql_skip_user_profile_image_extensions_enabled: false,
+          responsive_web_graphql_timeline_navigation_enabled: true,
+        },
+      },
+    })
+    return response
+  }
+
+  public static async getUserByScreenName(
+    screenName: string,
+    headers: TwitterAuthGuestTokenHeaders,
+  ) {
+    const url = 'graphql/sLVLhk0bGj3MVFEKTdax1w/UserByScreenName'
+    const response = await axios.get(url, {
+      baseURL: TWITTER_API_URL,
+      headers,
+      params: {
+        variables: {
+          screen_name: screenName,
+          withSafetyModeUserFields: true,
+        },
+        features: {
+          blue_business_profile_image_shape_enabled: true,
+          responsive_web_graphql_exclude_directive_enabled: true,
+          verified_phone_label_enabled: false,
+          responsive_web_graphql_skip_user_profile_image_extensions_enabled: false,
+          responsive_web_graphql_timeline_navigation_enabled: true,
+        },
+      },
+    })
+    return response
+  }
+
   public static async getUserTweets(
     userId: string,
     headers: TwitterAuthGuestTokenHeaders,
@@ -63,7 +113,6 @@ export class TwitterGraphqlApi {
           withVoice: true,
           withV2Timeline: true,
         },
-
         features: {
           blue_business_profile_image_shape_enabled: true,
           responsive_web_graphql_exclude_directive_enabled: true,
