edab4bf77e8d5851599759aacfe3f2a2fc2be025
HitomaruKonpaku <hunteratthenight@gmail.com>
2023-08-04 20:03:03 +0700

    Update holodex video

--- src/module/holodex/service/controller/holodex-video-controller.service.ts
+++ src/module/holodex/service/controller/holodex-video-controller.service.ts
@@ -1,5 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { baseLogger } from '../../../../logger'
+import { HolodexEntityUtil } from '../../util/holodex-entity.util'
+import { HolodexApiService } from '../api/holodex-api.service'
 import { HolodexVideoService } from '../data/holodex-video.service'
 
 @Injectable()
@@ -9,5 +11,13 @@ export class HolodexVideoControllerService {
   constructor(
     @Inject(HolodexVideoService)
     private readonly holodexVideoService: HolodexVideoService,
+    @Inject(HolodexApiService)
+    private readonly holodexApiService: HolodexApiService,
   ) { }
+
+  public async getVideoById(id: string) {
+    const { data } = await this.holodexApiService.getVideoById(id)
+    const video = await this.holodexVideoService.save(HolodexEntityUtil.buildVideo(data))
+    return video
+  }
 }
--- src/module/holodex/util/holodex-entity.util.ts
+++ src/module/holodex/util/holodex-entity.util.ts
@@ -1,4 +1,6 @@
+import { HolodexVideoType } from '../enum/holodex-video-type.enum'
 import { HolodexChannel } from '../model/holodex-channel.entity'
+import { HolodexVideo } from '../model/holodex-video.entity'
 
 export class HolodexEntityUtil {
   public static buildChannel(data: any) {
@@ -12,6 +14,28 @@ export class HolodexEntityUtil {
       org: data.org,
       suborg: data.suborg,
       group: data.group,
+    }
+    return obj
+  }
+
+  public static buildVideo(data: any) {
+    const obj: HolodexVideo = {
+      id: data.id,
+      createdAt: 0,
+      updatedAt: Date.now(),
+      channelId: data.channel.id,
+      type: data.type,
+    }
+    return obj
+  }
+
+  public static buildVideoPlaceholder(data: any) {
+    const obj: HolodexVideo = {
+      id: data.id,
+      createdAt: 0,
+      updatedAt: Date.now(),
+      channelId: data.channel_id,
+      type: HolodexVideoType.PLACEHOLDER,
     }
     return obj
   }
